494c32b4dc518e996383a91b6fc32b04711dea01
lovick <lovickk@gmail.com>
2018-02-14 09:48:09 -0800

    Initial setup, and setup the parsing for individual coin data.

---
+++ indivCoin.py
@@ -0,0 +1,25 @@
+import csv
+
+dic = {}
+
+#with open('crypto-markets.csv', newline='') as f:
+#    reader = csv.reader(f, delimiter=',', quotechar='"')
+#    for row in reader:
+        #dic[row[3]] = dic.get(row[3], 0) + 1
+coinSymbol = "BTC"
+desiredValues = [1, 2, 3, 5, 6, 7, 8]
+out = open(coinSymbol+'Data.csv', 'w+')
+with open('crypto-markets.csv', newline='') as f:
+    reader = csv.reader(f, delimiter=',', quotechar='"')
+    firstLoop = True
+    for row in reader:
+        if firstLoop:
+            for val in desiredValues[:-1]:
+                out.write(row[val]+',')
+            out.write(row[desiredValues[-1]]+'\n')
+            firstLoop = False
+        if row[1] == coinSymbol:
+            for val in desiredValues[:-1]:
+                out.write(row[val]+',')
+            out.write(row[desiredValues[-1]]+'\n')
+out.close()
