a36656f86bc8519bf84f6588a4b0fd0d8558d642
Sven Olson <lovickk@gmail.com>
2018-02-25 20:07:09 -0800

    Converted to candlestick plot.

--- scripts/largeGraph.js
+++ scripts/largeGraph.js
@@ -1,6 +1,6 @@
 function vis1() {
     var margin = {top: 30, right: 50, bottom: 50, left: 50},
-        width = (innerWidth - margin.left - margin.right)*.5,
+        width = (innerWidth - margin.left - margin.right),
         height = (innerHeight - margin.top - margin.bottom);
 
     var parseDate = d3.timeParse("%Y-%m-%d");
@@ -8,13 +8,21 @@ function vis1() {
     var y = d3.scaleLinear().rangeRound([height, 0]);
     var line = d3.line()
         .x(function(d) { return x(parseDate(d.date)); })
-        .y(function(d) { return y(+d.close); });
+        .y(function(d) { return y(+d.open); });
 
     var vVal = d => +d.close;
 
-    var c = d3.scaleLog()
+    var cOld = d3.scaleLog()
         .range(["steelblue", "crimson"])
         .interpolate(d3.interpolateHcl);
+
+    var c = function(d) {
+        if (+d.close > +d.open) {
+            return "#00FF00";
+        } else {
+            return "#ff0000";
+        }
+    }
 
     var xAxis = d3.axisBottom(x);
     var yAxis = d3.axisLeft(y);
@@ -29,17 +37,30 @@ function vis1() {
         console.log(d3.extent(data, function (d) { return parseDate(d.date); }));
         x.domain(d3.extent(data, function(d) { return parseDate(d.date); }));
         y.domain(d3.extent(data, function(d) { return +d.close; }));
-        c.domain(d3.extent(data, function(d) { return +d.close; }));
+        cOld.domain(d3.extent(data, function(d) { return +d.close; }));
 
         // Adding lines
         foreground = vis1.append("g")
             .attr("class", "foreground")
-            .selectAll("path")
-            .data([data])
-            .enter().append("path")
-            .attr("d", line)
+            .selectAll("rect")
+            .data(data)
+            .enter().append("rect")
+            .attr("x", function (d) {
+                return x(parseDate(d.date)) - 2 /* Adjust this later to be dynamic width */;
+            })
+            .attr("y", function (d) {
+                if (+d.close > +d.open) {
+                    return y(+d.close);
+                } else {
+                    return y(+d.open);
+                }
+            })
+            .attr("height", function (d) {
+                return Math.max(Math.abs( y(+d.close) - y(+d.open) ), 1);
+            })
+            .attr("width", "1px")
             .attr("stroke", function(d) {
-                return c(vVal(d));
+                return c(d);
             });
 
         vis1.append("g")
